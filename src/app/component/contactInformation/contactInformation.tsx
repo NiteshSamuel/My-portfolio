56eade4be642026189a01c803601069b88645db8
NiteshSamuel <niteshjsamuel@gmail.com>
2025-09-24 16:13:28 +0530

    some changes

---
+++ src/app/component/contactInformation/contactInformation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contactInformation = () => {
+  return (
+    <div>
+      <h1>Contact Information</h1>
+    </div>
+  )
+}
+
+export default contactInformation
---
+++ src/app/component/projectCard/projectCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const projectCard = () => {
+  return (
+    <div>
+      <h1>prject cards</h1>
+    </div>
+  )
+}
+
+export default projectCard
